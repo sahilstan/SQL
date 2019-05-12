fff1a2c89b7ba60c194be0c0a56e21f741e38fab
Sahil Sharma <me@iamsahilsharma.com>
2019-05-13 00:33:01 +0400

    Insertion done

--- tutorial.sql
+++ tutorial.sql
@@ -1,7 +1,7 @@
 CREATE TABLE students (
-    student_id INT,
-    name VARCHAR(20),
-    major VARCHAR(20),
+    student_id INT AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL,
+    major VARCHAR(20) DEFAULT 'Demo',
     PRIMARY KEY(student_id)
 );
 
@@ -26,3 +26,18 @@ INSERT INTO students(student_id, name) values(3, 'Naman');
 -- Cannot enter suplicate entries for primary key
 
 SELECT * FROM students;
+
+-- Drop the table start again
+
+DROP TABLE students;
+
+INSERT INTO students(name, major) values('Demo', 'Maths');
+
+INSERT INTO students(name, major) values('Demo2', 'CS');
+
+-- not null cannot be null
+
+INSERT INTO students values(3, 'Demo3', 'Bio');
+
+-- unique cannot be same for two rows 
+
